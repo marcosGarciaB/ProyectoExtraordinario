89f889c8bee3fe89f239a39e71603b85980f6068
marcosGarciaB <marckitosgarcia2003@gmail.com>
2025-06-02 21:09:59 +0200

    Haciendo el home.

--- app/src/main/java/com/example/proyectoextraordinario/FragmentHome.java
+++ app/src/main/java/com/example/proyectoextraordinario/FragmentHome.java
@@ -1,35 +1,48 @@
 package com.example.proyectoextraordinario;
 
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.MediaController;
+import android.widget.VideoView;
+
 public class FragmentHome extends Fragment {
+
+    private VideoView videoView;
+    private MediaController mediaController;
 
     public FragmentHome() {
         // Required empty public constructor
     }
 
-    public static FragmentHome newInstance(String param1, String param2) {
-        FragmentHome fragment = new FragmentHome();
-        Bundle args = new Bundle();
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        videoView = view.findViewById(R.id.videoViewID);
+        mediaController = new MediaController(getContext());
+
+        String path = "android.resource://" + requireActivity().getPackageName() + "/" + R.raw.video0;
+        Uri uri = Uri.parse(path);
+        videoView.setVideoURI(uri);
+
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+        videoView.start();
+
+        return view;
     }
 }
